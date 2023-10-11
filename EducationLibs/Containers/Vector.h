b279acf77fa3737f5fbc8af3e9a636f97448f5a0
Vladimir Aleksandrov <62762208+aleksvladimir@users.noreply.github.com>
2023-10-11 19:39:27 +0300

    Was added new class Vector with stubs of methods. - added new class Vector with stubs of methods - added new test-case Test_MyVector for class Vector - Note: new test-case hasn't passed yet

---
+++ EducationLibs/Containers/Vector.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+#include <stdexcept>
+
+template<typename T>
+class Vector
+{
+  T * buf_ = nullptr;
+  size_t size_ = 0;
+  size_t capacity_ = 0;
+public:
+
+  void push_back( const T& elem )
+  {
+    
+  }
+  void push_back( T && elem )
+  {
+
+  }
+  void pop_back()
+  {
+    
+  }
+  size_t capacity() const noexcept
+  {
+    return capacity_;
+  }
+  size_t size() const noexcept
+  {
+    return size_;
+  }
+  T& at( size_t i ) 
+  {
+    if ( i < size() )
+      return buf_[ i ];
+    throw std::out_of_range("");
+  }
+  const T & at( size_t i ) const
+  {
+    if ( i < size() )
+      return buf_[ i ];
+    throw std::out_of_range();
+  }
+  bool empty() const noexcept
+  {
+    return size() == 0;
+  }
+  void reserve( size_t newCapacity ) noexcept
+  {
+    capacity_ = newCapacity;
+  }
+  void clear()
+  {
+    
+  }
+  void shrink_to_fit()
+  {
+    
+  }
+  void resize( size_t newSize ) noexcept
+  {
+    size_ = newSize;
+  }
+  void resize( size_t newSize, const T & val ) noexcept
+  {
+    size_ = newSize;
+  }
+};
--- Tests/tests_containers/ContainersTests.cpp
+++ Tests/tests_containers/ContainersTests.cpp
@@ -1,7 +1,7 @@
 #include <array>
 #include <vector>
 
-
+#include "../../EducationLibs/Containers/Vector.h"
 #include "CppUnitTest.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -30,7 +30,7 @@ namespace ContainersTests
 			// at
 
 			std::vector<int> vec;
-			vec.push_back( 3 );
+			vec.push_back( 3 );	// rvalue
 			Assert::AreEqual( 1, ( int )vec.capacity() );
 			vec.push_back( 2 );
 			Assert::AreEqual( 2, ( int )vec.capacity() );
@@ -38,7 +38,8 @@ namespace ContainersTests
 			Assert::AreEqual( 3, ( int )vec.capacity() );
 			vec.push_back( 0 );
 			Assert::AreEqual( 4, ( int )vec.capacity() );
-			vec.push_back( 0 );
+			int a = 0;
+			vec.push_back( a );	// lvalue
 			Assert::AreEqual( 6, ( int )vec.capacity() );
 			Assert::AreEqual( 5, ( int )vec.size() );
 			Assert::AreEqual( 3, ( int )vec.at(0) );
@@ -126,7 +127,106 @@ namespace ContainersTests
 
 		TEST_METHOD( Test_MyVector)
 		{
-			Assert::IsTrue( false );
+			// push_back
+			// capacity
+			// size
+			// at
+
+			Vector<int> vec;
+			vec.push_back( 3 );	// rvalue
+			Assert::AreEqual( 1, ( int )vec.capacity() );
+			vec.push_back( 2 );
+			Assert::AreEqual( 2, ( int )vec.capacity() );
+			vec.push_back( 1 );
+			Assert::AreEqual( 3, ( int )vec.capacity() );
+			vec.push_back( 0 );
+			Assert::AreEqual( 4, ( int )vec.capacity() );
+			int a = 0;
+			vec.push_back( a );	// lvalue
+			Assert::AreEqual( 6, ( int )vec.capacity() );
+			Assert::AreEqual( 5, ( int )vec.size() );
+			Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+			Assert::AreEqual( 2, ( int )vec.at( 1 ) );
+			Assert::AreEqual( 1, ( int )vec.at( 2 ) );
+			Assert::AreEqual( 0, ( int )vec.at( 3 ) );
+			//todo:
+			//struct  Functor {
+			//	std::vector<int> vec_;
+			//	void operator () () {
+			//		vec_.at( 5 );
+			//	}
+			//};
+			//Assert::ExpectException < std::out_of_range, Functor >( Functor{ vec } );
+
+			// pop_back
+			// empty
+
+			Assert::IsTrue( !vec.empty() );
+			vec.pop_back();
+			Assert::AreEqual( 4, ( int )vec.size() );
+			vec.pop_back();
+			Assert::AreEqual( 3, ( int )vec.size() );
+			vec.pop_back();
+			Assert::AreEqual( 2, ( int )vec.size() );
+			vec.pop_back();
+			Assert::AreEqual( 1, ( int )vec.size() );
+			vec.pop_back();
+			Assert::AreEqual( 0, ( int )vec.size() );
+			Assert::AreEqual( 6, ( int )vec.capacity() );
+			Assert::IsTrue( vec.empty() );
+
+			// reserve (about capacity)
+
+			vec.reserve( 8 );
+			Assert::AreEqual( 8, ( int )vec.capacity() );
+			vec.reserve( 6 );															// doesn't reduce as shrink_to_fit!
+			Assert::AreEqual( 8, ( int )vec.capacity() );	// doesn't reduce as shrink_to_fit!
+			vec.push_back( 10 );
+			Assert::AreEqual( 1, ( int )vec.size() );
+			Assert::AreEqual( 10, ( int )vec.at( 0 ) );
+			vec.reserve( 16 );
+			Assert::AreEqual( 16, ( int )vec.capacity() );
+			Assert::AreEqual( 1, ( int )vec.size() );
+			Assert::AreEqual( 10, ( int )vec.at( 0 ) );
+
+			// clear
+
+			vec.clear();
+			Assert::AreEqual( 16, ( int )vec.capacity() );
+			Assert::AreEqual( 0, ( int )vec.size() );
+			vec.shrink_to_fit();
+			Assert::AreEqual( 0, ( int )vec.capacity() );
+			Assert::IsTrue( vec.empty() );
+
+			// shrink_to_fit (C++11)
+
+			vec.push_back( 11 );
+			vec.push_back( 12 );
+			vec.push_back( 13 );
+			Assert::AreEqual( 3, ( int )vec.size() );
+			vec.shrink_to_fit();
+			Assert::AreEqual( 3, ( int )vec.capacity() );
+
+			//resize (about size)
+			vec.resize( 1 );
+			Assert::AreEqual( 1, ( int )vec.size() );
+			Assert::AreEqual( 3, ( int )vec.capacity() );
+			Assert::AreEqual( 11, ( int )vec.at( 0 ) );
+			vec.resize( 5 );
+			Assert::AreEqual( 5, ( int )vec.size() );
+			Assert::AreEqual( 5, ( int )vec.capacity() );
+			Assert::AreEqual( 11, ( int )vec.at( 0 ) );
+			vec.resize( 1, 3 );
+			Assert::AreEqual( 1, ( int )vec.size() );
+			Assert::AreEqual( 5, ( int )vec.capacity() );
+			Assert::AreEqual( 11, ( int )vec.at( 0 ) );
+			vec.resize( 7, 66 );
+			Assert::AreEqual( 7, ( int )vec.size() );
+			Assert::AreEqual( 7, ( int )vec.capacity() );
+			Assert::AreEqual( 11, ( int )vec.at( 0 ) );
+			Assert::AreEqual( 66, ( int )vec.at( 6 ) );
+			Assert::AreEqual( 66, ( int )vec.at( 6 ) );
+			Assert::AreEqual( 66, ( int )vec.at( 6 ) );
 		}
 
 		TEST_METHOD( Test_std_list )
