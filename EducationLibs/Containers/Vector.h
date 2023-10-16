79831d8bb50bf702eae43f2c7f54d62a6816b41c
Vladimir Aleksandrov <62762208+aleksvladimir@users.noreply.github.com>
2023-10-16 23:44:59 +0300

    Improved vector clear().

--- EducationLibs/Containers/Vector.h
+++ EducationLibs/Containers/Vector.h
@@ -98,9 +98,8 @@ public:
   }
   void clear()
   {
-    for ( int i = 0; i < size_; ++i )
-      data_[ i ].~T();
-    size_ = 0;
+    while ( size_ > 0 )
+      pop_back();
   }
   void shrink_to_fit()
   {
