0059aecd30f7f021df6ae22e154c248e3b448db2
Vladimir Aleksandrov <62762208+aleksvladimir@users.noreply.github.com>
2023-10-25 20:28:53 +0300

    Was added deque by linked list. - added new test-case - added new class deque

--- EducationLibs/Containers/Deque.h
+++ EducationLibs/Containers/Deque.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <stdexcept>
 
 // taken from https://github.com/thaliaarchi/deque/tree/master#circular-array-algorithm
 namespace circular_array_algorithm
@@ -95,12 +96,180 @@ namespace circular_array_algorithm
   };  
 }
 
-namespace dec_as_doubly_linked_list
+namespace like_linked_list
 {
   /*
+   *  push_back(), pop_back(), push_front(), clear(), size(), resize(), empty(), pop_front()
+   *  shrink_to_fit()
    * todo:
-   *  push_back(), pop_back(), push_front(), pop_front()
-   *  clear(), resize(), shrink_to_fit()
-   *  size(), empty(), capacity(), at()
+   *  
+   *  at(), []
    */
+
+   /**
+    * \brief Deque by Doubly Linked List
+    * \tparam T - type_value
+    */
+  template<typename T>
+  class Deque
+  {
+    template<typename T>
+    struct Node
+    {
+      Node() = default;
+      Node( T val ) noexcept : value( val ) {}
+      T value = T();
+      Node<T> * next = nullptr;
+      Node<T> * prev = nullptr;
+    };
+
+    Node<T> * head_ = nullptr;
+    Node<T> * tail_ = nullptr;
+    size_t size_ = 0;
+
+  public:
+
+    Deque() = default;
+    Deque( size_t newSize, T && value = T() )
+    {
+      resize( newSize, value );
+    }
+    Deque( const Deque & other )
+    {
+      head_ = other.head_;
+      tail_ = other.tail_;
+      size_ = other.size_;
+    }
+    ~Deque()
+    {
+      if ( size() )
+        clear();
+    }
+    void push_back( const T & value )
+    {
+      if ( size_++ == 0 )
+        head_ = tail_ = new Node<T>( value );
+      else
+      {
+        auto p = new Node<T>( value );
+        tail_->next = p;
+        p->prev = tail_;
+        tail_ = p;
+      }
+    }
+    void push_front( const T & value )
+    {
+      if ( size_++ == 0 )
+        head_ = tail_ = new Node<T>( value );
+      else
+      {
+        auto p = new Node<T>( value );
+        head_->prev = p;
+        p->next = head_;
+        head_ = p;
+      }
+    }
+    void pop_back()
+    {
+      auto prev = tail_->prev;
+      delete tail_;
+      tail_ = prev;
+      --size_;
+    }
+    void pop_front()
+    {
+      auto next = head_->next;
+      delete head_;
+      head_ = next;
+      --size_;
+    }
+    T & back()
+    {
+      return tail_->value;
+    }
+    const T & back() const
+    {
+      return tail_->value;
+    }
+    T & front()
+    {
+      return head_->value;
+    }
+    const T & front() const
+    {
+      return head_->value;
+    }
+    [[nodiscard]] size_t size() const noexcept
+    {
+      return size_;
+    }
+    bool empty() const noexcept
+    {
+      return size_ == 0;
+    }
+    void clear()
+    {
+      while ( tail_->prev )
+        pop_back();
+      delete head_;
+      --size_;
+    }
+    void resize( size_t newSize, const T & val = T() )
+    {
+      while ( newSize < size_ )
+        pop_back();
+      while ( newSize > size_ )
+        push_back( val );
+    }
+    // O(N)!!!
+    T & at( size_t i )
+    {
+      return findNode( i )->value;
+    }
+    // O(N)!!!
+    const T & at( size_t i ) const
+    {
+      return findNode( i )->value;
+    }
+    // O(N)!!!
+    T & operator [] ( size_t i )
+    {
+      return findNode( i )->value;
+    }
+    // O(N)!!!
+    const T & operator [] ( size_t i ) const
+    {
+      return findNode( i )->value;
+    }
+
+  private:
+    Node<T>* findNode( size_t i )
+    {
+      if ( i >= size_ )
+        throw std::out_of_range( "" );
+
+      auto cnt = 0u;
+      Node<T> * node = head_;
+      while ( node->next && cnt < i )
+      {
+        node = node->next;
+        cnt++;
+      }
+      return node;
+    }
+    Node<T> const * findNode( size_t i ) const
+    {
+      if ( i >= size_ )
+        throw std::out_of_range( "" );
+
+      auto cnt = 0u;
+      Node<T> * node = head_;
+      while ( node->next && cnt < i )
+      {
+        node = node->next;
+        cnt++;
+      }
+      return node;
+    }
+  };
 }
--- Tests/tests_containers/ContainersTests.cpp
+++ Tests/tests_containers/ContainersTests.cpp
@@ -593,147 +593,141 @@ namespace ContainersTests
 				}
 			}
 
-			//// default_ctr
-			//// pop_back
-			//// empty
-			//{
-			//	Vector<int> vec( 4 );
-			//	Assert::AreEqual( 4, ( int )vec.size() );
-			//	Assert::AreEqual( 4, ( int )vec.capacity() );
-			//	Assert::IsTrue( !vec.empty() );
-			//	vec.pop_back();
-			//	Assert::AreEqual( 3, ( int )vec.size() );
-			//	vec.pop_back();
-			//	Assert::AreEqual( 2, ( int )vec.size() );
-			//	vec.pop_back();
-			//	Assert::AreEqual( 1, ( int )vec.size() );
-			//	vec.pop_back();
-			//	vec.pop_back();
-			//	Assert::AreEqual( 0, ( int )vec.size() );
-			//	Assert::AreEqual( 4, ( int )vec.capacity() );
-			//	Assert::IsTrue( vec.empty() );
-			//}
+			// default_ctr
+			// push_back
+			// push_front
+			// size
+			// at
+			{
+				like_linked_list::Deque<int> deque;
+				deque.push_back( 3 );
+				deque.push_back( 4 );
+				deque.push_back( 5 );
+				deque.push_front( 2 );
+				deque.push_front( 1 );
+				deque.push_front( 0 );
+				for ( int i = 0; i < deque.size(); ++i )
+				{
+					Assert::AreEqual( i, ( int )deque[ i ] );
+					Assert::AreEqual( i, ( int )deque.at( i ) );
+				}
+			}
 
-			//// reserve (about capacity)
-			//{
-			//	Vector<int> vec;
-			//	vec.reserve( 8 );
-			//	Assert::AreEqual( 8, ( int )vec.capacity() );
-			//	vec.reserve( 6 );															// doesn't reduce as shrink_to_fit!
-			//	Assert::AreEqual( 8, ( int )vec.capacity() );	// doesn't reduce as shrink_to_fit!
-			//	vec.push_back( 10 );
-			//	Assert::AreEqual( 1, ( int )vec.size() );
-			//	Assert::AreEqual( 10, ( int )vec.at( 0 ) );
-			//	vec.reserve( 16 );
-			//	Assert::AreEqual( 16, ( int )vec.capacity() );
-			//	Assert::AreEqual( 1, ( int )vec.size() );
-			//	Assert::AreEqual( 10, ( int )vec.at( 0 ) );
-			//}
+			// default_ctr
+			// push_back
+			// capacity
+			// size
+			// at
+			{
+				like_linked_list::Deque<int> vec;
+				vec.push_back( 3 );	// rvalue
+				vec.push_back( 2 );
+				vec.push_back( 1 );
+				int a = 0;
+				vec.push_back( a );	// lvalue
+				Assert::AreEqual( 4, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 2, ( int )vec.at( 1 ) );
+				Assert::AreEqual( 1, ( int )vec.at( 2 ) );
+				Assert::AreEqual( 0, ( int )vec.at( 3 ) );
+			}
 
-			//// clear
-			//{
-			//	Vector<int> vec( 16, 3 );
-			//	vec.clear();
-			//	Assert::AreEqual( 16, ( int )vec.capacity() );
-			//	Assert::AreEqual( 0, ( int )vec.size() );
-			//}
+			// default_ctr
+			// pop_back
+			// empty
+			{
+				like_linked_list::Deque<int> vec( 4 );
+				Assert::AreEqual( 4, ( int )vec.size() );
+				Assert::IsTrue( !vec.empty() );
+				vec.pop_back();
+				Assert::AreEqual( 3, ( int )vec.size() );
+				vec.pop_back();
+				Assert::AreEqual( 2, ( int )vec.size() );
+				vec.pop_back();
+				Assert::AreEqual( 1, ( int )vec.size() );
+				vec.pop_back();
+				Assert::AreEqual( 0, ( int )vec.size() );
+				Assert::IsTrue( vec.empty() );
+			}
+			// clear
+			{
+				like_linked_list::Deque<int> vec( 16, 3 );
+				vec.clear();
+				Assert::AreEqual( 0, ( int )vec.size() );
+			}
+			//resize (about size)
+			{
+				like_linked_list::Deque<int> vec;
+				vec.resize( 1 );
+				Assert::AreEqual( 1, ( int )vec.size() );
+				vec.push_back( 11 );
+				Assert::AreEqual( 2, ( int )vec.size() );
+				Assert::AreEqual( 0, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 11, ( int )vec.at( 1 ) );
+			}
+			{
+				like_linked_list::Deque<int> vec( 2, 3 );
+				Assert::AreEqual( 2, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 3, ( int )vec.at( 1 ) );
+				vec.resize( 5 );
+				Assert::AreEqual( 5, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 3, ( int )vec.at( 1 ) );
+				Assert::AreEqual( 0, ( int )vec.at( 2 ) );
+				Assert::AreEqual( 0, ( int )vec.at( 3 ) );
+				Assert::AreEqual( 0, ( int )vec.at( 4 ) );
+			}
+			{
+				like_linked_list::Deque<int> vec( 5, 3 );
+				Assert::AreEqual( 5, ( int )vec.size() );
 
-			//// shrink_to_fit (C++11)
-			//{
-			//	Vector<int> vec;
-			//	vec.reserve( 16 );
-			//	Assert::AreEqual( 0, ( int )vec.size() );
-			//	vec.push_back( 11 );
-			//	vec.push_back( 12 );
-			//	vec.push_back( 13 );
-			//	Assert::AreEqual( 3, ( int )vec.size() );
-			//	Assert::AreEqual( 16, ( int )vec.capacity() );
-			//	vec.shrink_to_fit();
-			//	Assert::AreEqual( 3, ( int )vec.capacity() );
-			//	vec.clear();
-			//	vec.shrink_to_fit();
-			//	Assert::AreEqual( 0, ( int )vec.capacity() );
-			//}
+				vec.resize( 1, 3 );
+				Assert::AreEqual( 1, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
 
-			////resize (about size)
-			//{
-			//	Vector<int> vec;
-			//	vec.resize( 1 );
-			//	Assert::AreEqual( 1, ( int )vec.size() );
-			//	Assert::AreEqual( 1, ( int )vec.capacity() );
-			//	vec.push_back( 11 );
-			//	Assert::AreEqual( 2, ( int )vec.size() );
-			//	Assert::AreEqual( 2, ( int )vec.capacity() );
-			//	Assert::AreEqual( 0, ( int )vec.at( 0 ) );
-			//	Assert::AreEqual( 11, ( int )vec.at( 1 ) );
-			//}
-			//{
-			//	Vector<int> vec( 2, 3 );
-			//	Assert::AreEqual( 2, ( int )vec.size() );
-			//	Assert::AreEqual( 2, ( int )vec.capacity() );
-			//	Assert::AreEqual( 3, ( int )vec.at( 0 ) );
-			//	Assert::AreEqual( 3, ( int )vec.at( 1 ) );
-			//	vec.resize( 5 );
-			//	Assert::AreEqual( 5, ( int )vec.size() );
-			//	Assert::AreEqual( 5, ( int )vec.capacity() );
-			//	Assert::AreEqual( 3, ( int )vec.at( 0 ) );
-			//	Assert::AreEqual( 3, ( int )vec.at( 1 ) );
-			//}
-			//{
-			//	Vector<int> vec( 5, 3 );
-			//	Assert::AreEqual( 5, ( int )vec.size() );
-			//	Assert::AreEqual( 5, ( int )vec.capacity() );
+				vec.resize( 3, 66 );
+				Assert::AreEqual( 3, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 66, ( int )vec.at( 1 ) );
+				Assert::AreEqual( 66, ( int )vec.at( 2 ) );
 
-			//	vec.resize( 1, 3 );
-			//	Assert::AreEqual( 1, ( int )vec.size() );
-			//	Assert::AreEqual( 5, ( int )vec.capacity() );
-			//	Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				vec.resize( 7, 77 );
+				Assert::AreEqual( 7, ( int )vec.size() );
+				Assert::AreEqual( 3, ( int )vec.at( 0 ) );
+				Assert::AreEqual( 66, ( int )vec.at( 1 ) );
+				Assert::AreEqual( 66, ( int )vec.at( 2 ) );
+				Assert::AreEqual( 77, ( int )vec.at( 3 ) );
+				Assert::AreEqual( 77, ( int )vec.at( 4 ) );
+				Assert::AreEqual( 77, ( int )vec.at( 5 ) );
+				Assert::AreEqual( 77, ( int )vec.at( 6 ) );
+			}
+			// []
+			{
+				like_linked_list::Deque<int> vec( 3 );
+				vec.push_back( 4 );
+				vec.push_back( 6 );
+				Assert::AreEqual( 4, ( int )vec[ 3 ] );
+				Assert::AreEqual( 6, ( int )vec[ 4 ] );
+				const like_linked_list::Deque<int> const_vec( 5 );
+				auto & elem = const_vec[ 4 ];
+				//elem = 3;	// error: const elem
+			}
 
-			//	vec.resize( 3, 66 );
-			//	Assert::AreEqual( 3, ( int )vec.size() );
-			//	Assert::AreEqual( 5, ( int )vec.capacity() );
-			//	Assert::AreEqual( 3, ( int )vec.at( 0 ) );
-			//	Assert::AreEqual( 66, ( int )vec.at( 1 ) );
-			//	Assert::AreEqual( 66, ( int )vec.at( 2 ) );
+			// back
+			{
+				like_linked_list::Deque<int> vec;
+				vec.push_back( 1 );
+				vec.push_back( 2 );
+				Assert::AreEqual( 2, ( int )vec.back() );
+			}
 
-			//	vec.resize( 7, 77 );
-			//	Assert::AreEqual( 7, ( int )vec.size() );
-			//	Assert::AreEqual( 7, ( int )vec.capacity() );
-			//	Assert::AreEqual( 3, ( int )vec.at( 0 ) );
-			//	Assert::AreEqual( 66, ( int )vec.at( 1 ) );
-			//	Assert::AreEqual( 66, ( int )vec.at( 2 ) );
-			//	Assert::AreEqual( 77, ( int )vec.at( 3 ) );
-			//	Assert::AreEqual( 77, ( int )vec.at( 4 ) );
-			//	Assert::AreEqual( 77, ( int )vec.at( 5 ) );
-			//	Assert::AreEqual( 77, ( int )vec.at( 6 ) );
-			//}
-			//// []
-			//{
-			//	Vector<int> vec( 3 );
-			//	vec.push_back( 4 );
-			//	Assert::AreEqual( 6, ( int )vec.capacity() );
-			//	vec.push_back( 6 );
-			//	Assert::AreEqual( 4, ( int )vec[ 3 ] );
-			//	Assert::AreEqual( 6, ( int )vec[ 4 ] );
-			//	const Vector<int> const_vec( 5 );
-			//	auto & elem = const_vec[ 4 ];
-			//	//elem = 3;	// error: const elem
-			//}
-
-			//// back
-			//{
-			//	Vector<int> vec;
-			//	vec.push_back( 1 );
-			//	vec.push_back( 2 );
-			//	Assert::AreEqual( 2, ( int )vec.back() );
-			//}
-
-			//// destructor
-			//{
-			//	Vector<int> vec( 3, 4 );
-			//	vec.~Vector();
-			//	Assert::IsTrue( vec.empty() );
-			//}
+			// destructor
+			{
+				like_linked_list::Deque<int> vec( 3, 4 );
+				vec.~Deque();
+				Assert::IsTrue( vec.empty() );
+			}
 		}
 
 		TEST_METHOD( Test_std_set )
